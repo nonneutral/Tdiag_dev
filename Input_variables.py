34ff9bfab0af903f796c50fc25e59584734672ae
Wanseo Kim <wk222@ic.ac.uk>
2026-01-13 14:45:39 +0000

    Revert "OOP 2nd attempt"
    This reverts commit b9c905188ed29cbb82434ac23f3e3e6aa36ab542, reversing
    changes made to d862d8476b3b1c0bfbcbf110bed30330f450ce41.

--- Input_variables.py
+++ Input_variables.py
@@ -6,25 +6,44 @@ from solver_copy import find_solution
 kb=1.38064852e-23 #Boltzmann's constant in joules per kelvin
 e0=8.854187817e-12 #farads per meter
 
+# particle properties
+m_e=9.1093837e-31 #electron mass in kilograms
+q_e=1.60217662e-19 #elementary charge in coulombs
+
 
 # plasma parameters
-T_e=1960 #plasma temperature in kelvin
 N_e=8e6 #number of electrons
-rad2=0.0002 #plasma radius in meters
-B=1.6 #magnetic field in tesla
+T_e=1960 #plasma temperature in kelvin
 freq_guess = 2.0e6  # obsolete
 omega_r  = 2*np.pi*freq_guess
-q_e=1.60217662e-19 #elementary charge in coulombs
-m_e=9.1093837e-31 #electron mass in kilograms
+rad2=0.0002 #plasma radius in meters
+B=1.6 #magnetic field in tesla
 
 # electrode parameters
-electrode_borders=[0.025,0.050,0.100,0.125]
+electrode_borders=np.array([0.025,0.050,0.100,0.125])
 initial_voltages=np.array([0,-50,-10,-50,0]) #excitations initial in volts
 final_voltages=np.array([0,-15,-10,-50,0], dtype=float) #excitations final in volts
 electrode_voltages=initial_voltages 
 rw=.017 #radius of inner wall of cylindrical electrodes, in meters
 Llim=0.035 #axial window limit, in meters
 Rlim=0.110
+
+"""np.array([N_e,
+    T_e,
+    freq_guess,
+    rad2,
+    B,
+    np.array([initial_voltages,electrode_borders]),
+    Llim,
+    Rlim,
+    40,
+    20,
+    3.0,
+    True,
+    20,
+    False,
+    'raise',
+    ''])"""
 
 
 # tolerances for bessel function expansions
