ec99fb66908657e223ffa710487b714ecfa1024b
cid02036225 <wk222@ic.ac.uk>
2026-04-09 21:36:39 +0200

    made plotting.py more quick and easy to use

--- plotting.py
+++ plotting.py
@@ -2,6 +2,13 @@ import os
 import re
 import numpy as np
 import matplotlib.pyplot as plt
+import lin_fit_optimiser as lfo
+
+def quadratic(x):
+    return x**2
+
+def quadinv(x):
+    return np.sqrt(x)
 
 d50mm = np.loadtxt("./useful_data/full_protocol_scan_d0.050.csv", delimiter=",")
 d55mm = np.loadtxt("./useful_data/full_protocol_scan_d0.055.csv", delimiter=",")
@@ -76,11 +83,10 @@ Temperature_list_str =["T 100 K",
                 "T 1900 K",
                 "T 2000 K"
                 ]
-fig, axs = plt.subplots(2, 2, figsize=(12,10))
+#fig2,axs2 = plt.figure()
 
 # ---------------- Length investigation ----------------
-err_list_d = []
-
+"""
 for i in range(len(distance_list)):
     data = distance_list[i]
     distance = distance_list_str[i]
@@ -112,49 +118,160 @@ axs[0,1].plot([50,55,60,65,70,75,85,95,105],
               linestyle="-")
 
 axs[0,1].set_xlabel("Length between inner electrodes (mm)",fontsize=18)
-axs[0,1].set_ylabel(r"$\sigma \, (V^{-1})$",fontsize=18)
+axs[0,1].set_yscale("log")
+axs[0,1].set_ylabel(r"Square-fit error: $\sigma \, (V^{-1})$",fontsize=18)
 
 
 # ---------------- Temperature investigation ----------------
+"""
+
+q_e=1.60217662e-19 #electron charge in coulombs
+kb = kb=1.38064852e-23 #Boltzmann's constant in joules per kelvin
+
+#fig1, axs1 = plt.subplots(1, 2, figsize=(20,10))
+fig2, axs2 = plt.subplots(1, 2, figsize=(20,10))
+
+
 err_list = []
+T_fit_list = []
 
 for j in range(len(temperature_list)):
     data = temperature_list[j]
     temperature = Temperature_list_str[j]
 
+    crop_factor = 1
+    
     escaped_list = data[0,:]
     vacdrop_list = data[1,:]
     drop_list = data[2,:]
+    xaxis = vacdrop_list
 
-    fit, cov = np.polyfit(vacdrop_list, np.log(escaped_list), deg=1, cov=True)
-    polyval = np.polyval(fit, vacdrop_list)
+    fit, cov = np.polyfit(xaxis, np.log(escaped_list), deg=1, cov=True)
+    polyval = np.polyval(fit, xaxis)
+
+    T_estimate = -q_e*1.05/(kb*fit[0])
+    T_fit_list.append(T_estimate)
 
     err_list.append(cov[0,0])
-    if (j+1)%2:
-        axs[1,0].plot(vacdrop_list, np.log10(escaped_list),
-                    label=f"{temperature}",
-                    marker='o', linestyle='-', markersize=5, linewidth=1)
+    axs2[0].plot(xaxis, np.log10(escaped_list),
+                label=f"{temperature}",
+                marker='o', linestyle='-', markersize=5, linewidth=1)
 
-        axs[1,0].plot(vacdrop_list, polyval/np.log(10))
+    axs2[0].plot(xaxis, polyval/np.log(10))
 
-axs[1,0].set_xlabel("Confinement (V)",fontsize=18)
-axs[1,0].set_ylabel(r"$N_\text{esc}$ T investigation",fontsize=18)
-axs[1,0].legend()
+axs2[0].set_xlabel("Confinement (V)",fontsize=18)
+axs2[0].set_ylabel(r"$N_\text{esc}$ T investigation",fontsize=18)
+axs2[0].legend()
 
 #fig.text(0.5, 0.96, "Finite length investigation", ha='center', fontsize=16)
 #fig.text(0.5, 0.48, "Temperature investigation", ha='center', fontsize=16)
 
 # ---------------- Error vs temperature ----------------
-axs[1,1].plot(np.arange(100,2100,100),
+axs2[1].plot(np.arange(100,2100,100),
               err_list,
               marker='o',
               linestyle='-',
               markersize=5,
               linewidth=1)
 
-axs[1,1].set_xlabel(r"Temperature (K)",fontsize=18)
-axs[1,1].set_ylabel(r"$\sigma \, (V^{-1})$",fontsize=18)
+axs2[1].set_xlabel(r"Temperature (K)",fontsize=18)
+axs2[1].set_yscale("log")
+axs2[1].set_ylabel(r"Square-fit error: $\sigma \, (V^{-1})$",fontsize=18)
+plt.show()
 
+plt.plot(np.arange(100,2100,100),100*np.abs(np.arange(100,2100,100)-T_fit_list)/np.arange(100,2100,100), marker="o", linestyle="-",color="b")
+#plt.plot(np.arange(100,2100,100),np.arange(100,2100,100))
+plt.title("Percentage deviation of linear fit T from input T", fontsize=20)
+plt.xlabel("Input Temperature (K)", fontsize=20)
+plt.ylabel("Deviation of linear fit T from input T", fontsize=20)
+plt.show()
+
+
+fig, ax = plt.subplots(figsize=(10,6))  # create one figure and axis
+
+for k in range(len(temperature_list)):
+    data = temperature_list[k]
+    temperature = Temperature_list_str[k]
+
+    crop_factor = 1
+    
+    escaped_list = data[0,:]
+    vacdrop_list = data[1,:]
+    drop_list = data[2,:]
+
+    # Plot all datasets on the same axis
+    ax.plot(vacdrop_list, drop_list,
+            marker="o", linestyle="-",
+            label=f"T = {temperature} K")
+ax.plot(vacdrop_list, vacdrop_list, linestyle="--", color="black", label="y=x reference")  # Add a reference line y=x
+# Labels, legend, title
+ax.set_xlabel("Vacuum drop (V)", fontsize=18)
+ax.set_ylabel("Drop (V)", fontsize=18)
+ax.legend()
+ax.set_title("Drop vs Vacuum Drop", fontsize=20)
+plt.tight_layout()
+plt.show()
+
+"""
+err_list_d = []
+l_p_s = []
+crop_factor_list = []
+
+
+
+d_range=np.arange(0.040,0.105,0.005)
+for d in d_range:
+    data = np.loadtxt(f"useful_data_2/trial6_full_protocol_scan_d{d:.3f}.csv",delimiter=",")
+    data_label = str(f"d = {round(d*1000)} mm")
+
+    escaped_list = data[0,:]
+    vacdrop_list = data[1,:]
+    drop_list = data[2,:]
+    l_p_list = data[3,:]
+
+    crop_factor,_,_ = lfo.find_crop_factor(escaped_list, vacdrop_list, drop_list)
+    print(f"Crop factor for {data_label}: {crop_factor}")
+    crop_factor_list.append(crop_factor)
+    fit, cov = np.polyfit(vacdrop_list, np.log(escaped_list), deg=1, cov=True)
+    polyval = np.polyval(fit, vacdrop_list)
+
+    err_list_d.append(cov[0,0])
+
+    plasma_length = np.average(l_p_list[0])
+    #plt.plot(l_p_list)
+    l_p_s.append(plasma_length)
+    
+    if int(round(d * 1000)) % 10 == 0:
+        axs[0].plot(vacdrop_list, np.log10(escaped_list),
+                    label=data_label,
+                    marker='o', linestyle='-', markersize=7, linewidth=1)
+        axs[0].plot(vacdrop_list, polyval/np.log(10),color="black",linewidth = 3)
+
+axs[0].set_xlabel("Confinement (V)",fontsize=25)
+axs[0].set_ylabel(r"$N_\text{esc}$ L investigation",fontsize=25)
+axs[0].tick_params(axis='both', labelsize=20)
+axs[0].legend(fontsize=25)
+axs[0].grid(True)
+
+#fig.text(0.5, 0.96, "Finite length investigation", ha='center', fontsize=16)
+#fig.text(0.5, 0.48, "Temperature investigation", ha='center', fontsize=16)
+
+# ---------------- Error vs temperature ----------------
+axs[1].plot(np.array(d_range)*1000,
+              err_list_d,
+              marker='o',
+              linestyle='-',
+              markersize=7,
+              linewidth=3,color="b")
+axs[1].grid(True)
+axs[1].set_xlabel(r"electrodes distance (mm)",fontsize=25)
+axs[1].set_yscale("log")
+axs[1].set_ylabel(r"Square-fit error: $\sigma \, (V^{-1})$",fontsize=25)
+axs[1].tick_params(axis='both', labelsize=20)
 
 plt.tight_layout(rect=[0,0,1,0.95])
 plt.show()
+
+plt.scatter(d_range, l_p_s)
+
+print(crop_factor_list)"""
